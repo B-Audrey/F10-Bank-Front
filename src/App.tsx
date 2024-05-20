e664a0a7f8b01c73deec4450242a0178e615e8b0
audrey <audrey@devzone.fr>
2024-05-20 17:48:04 +0200

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/home.tsx';
+import Header from './components/header/header.tsx';
+import Footer from './components/footer/footer.tsx';
 
 function App() {
 
-    return (
-        <>
-            <Router>
-                <div>App Works</div>
-                {/*<Header />*/}
-                <Routes>
-                    <Route path='/' element={<Home />}/>
-                    {/*<Route path='/error' element={<WrongPage />}/>*/}
-                    {/*<Route path='*' element={<WrongPage />}/>*/}
-                </Routes>
-                {/*<Footer />*/}
-            </Router>
-        </>
-    );
+  return (
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/*<Route path='/error' element={<WrongPage />}/>*/}
+          {/*<Route path='*' element={<WrongPage />}/>*/}
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  );
 }
 
 export default App;
