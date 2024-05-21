7afe37d76b46d23792f1d55e7d067b919a05d83d
audrey <audrey@devzone.fr>
2024-05-21 16:41:00 +0200

    feat: logout

--- src/shared/components/header/header.tsx
+++ src/shared/components/header/header.tsx
@@ -1,8 +1,25 @@
 import { NavLink } from 'react-router-dom';
 import './header.scss';
+import { useEffect, useState } from 'react';
+import { useStore } from 'react-redux';
 
 export default function Header() {
-  return (
+  const [userName, setUserName] = useState('');
+
+  const logOut = () => {
+    store.dispatch({ type: 'LOGOUT' });
+  };
+
+  const store = useStore();
+
+  useEffect(() => {
+    store.subscribe(() => {
+      return setUserName(store.getState()['email']);
+    });
+    console.log(userName);
+  }, [store, userName]);
+
+  return !userName ? (
     <nav className="main-nav">
       <NavLink className="main-nav-logo" to="/">
         <img className="main-nav-logo-image" src="/assets/img/argentBankLogo.png" alt="Argent Bank Logo" />
@@ -12,6 +29,23 @@ export default function Header() {
         <NavLink className="main-nav-item" to="/login">
           <i className="fa fa-user-circle"></i>
           &nbsp;Sign In
+        </NavLink>
+      </div>
+    </nav>
+  ) : (
+    <nav className="main-nav">
+      <NavLink className="main-nav-logo" to="/">
+        <img className="main-nav-logo-image" src="/assets/img/argentBankLogo.png" alt="Argent Bank Logo" />
+        <h1 className="sr-only">Argent Bank</h1>
+      </NavLink>
+      <div>
+        <NavLink className="main-nav-item" to="./profile">
+          <i className="fa fa-user-circle"></i>
+          &nbsp;{userName.split('@')[0]}
+        </NavLink>
+        <NavLink onClick={logOut} className="main-nav-item" to="/">
+          <i className="fa fa-sign-out"></i>
+          Sign Out
         </NavLink>
       </div>
     </nav>
